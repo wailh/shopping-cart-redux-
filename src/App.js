b1e0b675f41640a8e1942b4d13b4f7e53c1c8b8c
wail hamdi <w@gmail.com>
2022-01-07 03:38:02 +0100

    second commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="grid-container">
+        <header>
+          <a href='/'>React Shopping Cart</a>
+        </header>
+        <main>Product List</main>
+        <footer>All Rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
